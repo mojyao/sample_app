a233766406a1153860118a5dc83653f193f1a365
EC2 Default User <ec2-user@ip-172-31-4-5.us-east-2.compute.internal>
2018-07-18 11:37:44 +0000

    Add user microposts

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,28 @@
+class MicropostsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+       @feed_items = []
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
+    
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
+    end
+end
